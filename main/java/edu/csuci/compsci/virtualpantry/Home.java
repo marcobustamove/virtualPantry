61aa4f2457e5d7f958e39d8cefce2fa04717c3eb
Marco Antonio Bustamante <marcoabbpm@hotmail.com>
2018-10-12 15:46:09 -0700

    pantryScreen
    Just created an activity and xml for it

---
+++ main/java/edu/csuci/compsci/virtualpantry/Home.java
@@ -0,0 +1,15 @@
+package edu.csuci.compsci.virtualpantry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
