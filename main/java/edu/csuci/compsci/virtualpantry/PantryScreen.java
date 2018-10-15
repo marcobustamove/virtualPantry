462d556b3f6d399b50a93cbd0b7ce3b4409379fa
Marco Antonio Bustamante <marcoabbpm@hotmail.com>
2018-10-15 14:57:09 -0700

    Just Setup pantryScreen

---
+++ main/java/edu/csuci/compsci/virtualpantry/PantryScreen.java
@@ -0,0 +1,15 @@
+package edu.csuci.compsci.virtualpantry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantryScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantry_screen);
+    }
+}
