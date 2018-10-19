58c594cab7c7b946510ced79e9c2df802a89691a
Christian Gutierrez <christian.gutierrez63@yahoo.com>
2018-10-19 16:26:28 -0700

    Added top and lower bar mockups to Item screen, corrected constraints on main RecyclerView

--- main/java/edu/csuci/compsci/virtualpantry/Home.java
+++ main/java/edu/csuci/compsci/virtualpantry/Home.java
@@ -17,12 +17,14 @@ public class Home extends AppCompatActivity implements MyRecyclerViewAdapter.Ite
         setContentView(R.layout.activity_items);
 
         // data to populate the RecyclerView with
-        //String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
+        String[] data = {"Apples", "Bananas", "Oranges", "Watermelon", "Peaches", "Kiwi", "Strawberries", "Grapes", "Avocado", "Pineapple", "Kiwano Melon", "Dragonfruit", "Tomato"};
+        /* test array for displaying 150 grid items
         String data[] = new String[150];
         for(int i = 0; i < data.length; i++)
         {
             data[i] = Integer.toString(i+1);
         }
+        */
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.rvNumbers);
         int numberOfColumns = 3;
