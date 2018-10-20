199439d24a65eee19118b139e7759c90873d7e92
Juan <juan.ramirez165@myci.csuci.edu>
2018-10-19 17:37:40 -0700

    Finished the login design

--- main/java/edu/csuci/compsci/virtualpantry/Home.java
+++ main/java/edu/csuci/compsci/virtualpantry/Home.java
@@ -10,6 +10,6 @@ public class Home extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_account);
+        setContentView(R.layout.login);
     }
 }
