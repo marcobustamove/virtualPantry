befcc254b20bb9c1287db1a465964d60a79a267d
Juan <juan.ramirez165@myci.csuci.edu>
2018-10-21 20:33:09 -0700

    Connected all the screens we have so far. I think.

--- main/java/edu/csuci/compsci/virtualpantry/LoginActivity.java
+++ main/java/edu/csuci/compsci/virtualpantry/LoginActivity.java
@@ -1,13 +1,35 @@
 package edu.csuci.compsci.virtualpantry;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button createAccountSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
+
+        createAccountSubmitButton = (Button) findViewById(R.id.create_account_submit_button);
+        createAccountSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginScreenActivity();
+            }
+
+        });
     }
+
+    public void openLoginScreenActivity()
+    {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
 }
+
