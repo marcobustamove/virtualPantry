45803055d20fe2676ec5d56f370631c04e10fe13
Marco Antonio Bustamante <marcoabbpm@hotmail.com>
2018-11-02 15:23:00 -0700

    Started Implementation of Pantry DB in HomeScreenActivity

--- main/java/database/PantryBaseHelper.java
+++ main/java/database/PantryBaseHelper.java
@@ -3,11 +3,16 @@ package database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.design.widget.TextInputEditText;
+
+import java.util.UUID;
+
+import database.PantryDBSchema.PantryTable;
 
 public class PantryBaseHelper extends SQLiteOpenHelper
 {
     private static final int VERSION = 1;
-    private static final String DATABASE_NAME = "crimeBase.db";
+    private static final String DATABASE_NAME = "PantryDatabase.db";
 
     public PantryBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
@@ -16,6 +21,10 @@ public class PantryBaseHelper extends SQLiteOpenHelper
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + PantryTable.NAME + "(" +
+        " _id integer primary key autoincrement, " +
+                PantryTable.Cols.UUID + ", " +
+                PantryTable.Cols.TITLE + ")");
 
     }
 
--- main/java/edu/csuci/compsci/virtualpantry/HomeScreenActivity.java
+++ main/java/edu/csuci/compsci/virtualpantry/HomeScreenActivity.java
@@ -1,33 +1,64 @@
 package edu.csuci.compsci.virtualpantry;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import database.PantryBaseHelper;
+
 public class HomeScreenActivity extends AppCompatActivity {
 
-    private Button open_pantry;
+
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    private Button openPantry;
+    private Button createPantry;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        open_pantry = (Button) findViewById(R.id.openpantry);
-        open_pantry.setOnClickListener(new View.OnClickListener() {
+        mContext = this.getApplicationContext();
+        mDatabase = new PantryBaseHelper(mContext).getWritableDatabase();
+
+        openPantry = (Button) findViewById(R.id.openpantry);
+        openPantry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openPantry();
+                openPantryScreen();
 
             }
         });
 
+        createPantry = (Button) findViewById(R.id.create_pantry_button);
+        createPantry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPantry();
+
+            }
+        });
+
+
+
     }
 
-    public void openPantry()
+    public void openPantryScreen()
     {
         Intent intent = new Intent(this, PantryScreenActivity.class);
         startActivity(intent);
     }
+
+    public void addPantry()
+    {
+        //TODO: Implement
+    }
+
+    //public #returnValue getPantry(UUID id){ return #returnValue;}
 }
