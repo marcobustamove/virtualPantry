9fc1ab5bae8ad8a23658ede60017db600062e30d
Juan <juan.ramirez165@myci.csuci.edu>
2018-11-05 13:42:05 -0800

    Added the drawer to the home screen.
    Not fully developed but got the functionality to work.

--- main/java/edu/csuci/compsci/virtualpantry/homeactivity.java
+++ main/java/edu/csuci/compsci/virtualpantry/homeactivity.java
@@ -1,14 +1,27 @@
 package edu.csuci.compsci.virtualpantry;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class homeactivity extends AppCompatActivity {
 
     private Button open_pantry;
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +36,34 @@ public class homeactivity extends AppCompatActivity {
             }
         });
 
+        mDrawerLayout= (DrawerLayout)findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
 
     public void openPantry()
     {
