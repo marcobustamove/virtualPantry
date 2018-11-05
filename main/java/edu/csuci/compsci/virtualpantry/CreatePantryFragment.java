3138e84bb15b1edda60f980a9ab0138d931126af
Marco Antonio Bustamante <marcoabbpm@hotmail.com>
2018-11-05 10:58:09 -0800

    Added dialog to add pantry

--- main/java/edu/csuci/compsci/virtualpantry/CreatePantryFragment.java
+++ main/java/edu/csuci/compsci/virtualpantry/CreatePantryFragment.java
@@ -1,5 +1,25 @@
 package edu.csuci.compsci.virtualpantry;
 
-public class CreatePantryFragment
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class CreatePantryFragment extends DialogFragment
 {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.fragment_createpantry, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.create_pantry)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
 }
