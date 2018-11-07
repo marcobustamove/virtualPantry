bbcdd2ba5a4a3c1d474b6061bedd7feae3c7dcce
Christian Gutierrez <christian.gutierrez63@yahoo.com>
2018-11-06 23:00:26 -0800

    AddItemDialog work
    Added cancel button to add item dialog, began working on passing data from dialog to DB

--- main/java/edu/csuci/compsci/virtualpantry/AddItemFragment.java
+++ main/java/edu/csuci/compsci/virtualpantry/AddItemFragment.java
@@ -2,6 +2,7 @@ package edu.csuci.compsci.virtualpantry;
 
 import android.app.Dialog;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
@@ -10,6 +11,7 @@ import android.view.View;
 
 public class AddItemFragment extends DialogFragment
 {
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -19,7 +21,11 @@ public class AddItemFragment extends DialogFragment
         return new AlertDialog.Builder(getActivity(), R.style.AddItemDialogTheme)
                 .setView(v)
                 .setTitle(R.string.addItemTitle)
-                .setPositiveButton(android.R.string.ok, null)
+                .setPositiveButton(R.string.addItemDone, null)
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        AddItemFragment.this.getDialog().cancel();
+                    }})
                 .create();
     }
 
