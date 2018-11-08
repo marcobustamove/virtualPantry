679569025bf320b20a55041efc24ec407db254c8
Juan <juan.ramirez165@myci.csuci.edu>
2018-11-07 16:56:10 -0800

    Working on filling the drawer with items from the database

--- main/java/edu/csuci/compsci/virtualpantry/homeactivity.java
+++ main/java/edu/csuci/compsci/virtualpantry/homeactivity.java
@@ -2,6 +2,9 @@ package edu.csuci.compsci.virtualpantry;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,8 +14,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+import java.util.List;
 
 public class homeactivity extends AppCompatActivity {
 
@@ -20,6 +29,12 @@ public class homeactivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private ArrayAdapter<String> mAdapter;
+    private ListView mDrawerList;
+
+    //CursorAdapter for binding to a sql
+    ListView listView;
+
 
 
     @Override
@@ -41,10 +56,43 @@ public class homeactivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
+        listView = (ListView) findViewById(R.id.simpleListView);
+
+
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        addMenuItemInNavMenuDrawer();
+
+
+
+
+
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        /*simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String>arrayAdapter = new ArrayAdapter<String>(this, R.layout.listview,R.id.textView,animalList);
+        simpleList.setAdapter(arrayAdapter);*/
 
 
+    }
+    private void addMenuItemInNavMenuDrawer() {
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
 
+        Menu menu = navView.getMenu();
+        //Menu submenu = menu.addSubMenu("New Super SubMenu");
+
+        menu.add("Super Item1");
+        menu.add("Super Item2");
+        menu.add("Super Item3");
+
+        navView.invalidate();
     }
 
     @Override
