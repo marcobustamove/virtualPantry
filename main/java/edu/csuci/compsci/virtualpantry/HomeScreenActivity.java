20b369b3ea70fd8e63f19e1930fbef5d9683ce42
Marco Antonio Bustamante <marcoabbpm@hotmail.com>
2018-11-07 17:39:12 -0800

    Fixing a couple merge issues - this is still a test branch

--- main/java/edu/csuci/compsci/virtualpantry/HomeScreenActivity.java
+++ main/java/edu/csuci/compsci/virtualpantry/HomeScreenActivity.java
@@ -4,10 +4,19 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import java.util.UUID;
 
@@ -24,6 +33,12 @@ public class HomeScreenActivity extends AppCompatActivity implements CreatePantr
     private Button mOpenPantry;
     private Button mCreatePantry;
     private String userInputPantryName;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private ArrayAdapter<String> mAdapter;
+    private ListView mDrawerList;
+    //CursorAdapter for binding to a sql
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -52,6 +67,21 @@ public class HomeScreenActivity extends AppCompatActivity implements CreatePantr
                 openCreatePantryDialog();
             }
         });
+
+
+        mDrawerLayout= (DrawerLayout)findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        listView = (ListView) findViewById(R.id.simpleListView);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        });
+        addMenuItemInNavMenuDrawer();
 
 
 
@@ -90,6 +120,25 @@ public class HomeScreenActivity extends AppCompatActivity implements CreatePantr
         mWritableDatabase.update(PantryTable.NAME, values,
                 PantryTable.Cols.UUID + " = ?",
                 new String[] { pantryID.toString() });
+    }
+
+    private void addMenuItemInNavMenuDrawer() {
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        Menu menu = navView.getMenu();
+        //Menu submenu = menu.addSubMenu("New Super SubMenu");
+        menu.add("Super Item1");
+        menu.add("Super Item2");
+        menu.add("Super Item3");
+        navView.invalidate();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
