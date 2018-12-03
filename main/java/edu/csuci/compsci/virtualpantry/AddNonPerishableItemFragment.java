c9d7f7907d0dd1a892d8445b0ff3603e3e34d99c
Marco Antonio Bustamante <marcoabbpm@hotmail.com>
2018-12-02 16:02:01 -0800

    Scraps

---
+++ main/java/edu/csuci/compsci/virtualpantry/AddNonPerishableItemFragment.java
@@ -0,0 +1,20 @@
+package edu.csuci.compsci.virtualpantry;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+
+public class AddNonPerishableItemFragment extends Fragment
+{
+
+}
---
+++ main/java/edu/csuci/compsci/virtualpantry/AddPerishableItemFragment.java
@@ -0,0 +1,5 @@
+package edu.csuci.compsci.virtualpantry;
+
+public class AddPerishableItemFragment
+{
+}
